7455731e623062b0c4432b47c9176a12942a4be2
MaeveWheaton <teamheaven10@outlook.com>
2021-06-03 09:28:03 -0400

    coded

--- ArrayExamples/Form1.Designer.cs
+++ ArrayExamples/Form1.Designer.cs
@@ -45,10 +45,10 @@ namespace ArrayExamples
             this.q3Output.BackColor = System.Drawing.Color.White;
             this.q3Output.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.q3Output.ForeColor = System.Drawing.Color.Black;
-            this.q3Output.Location = new System.Drawing.Point(29, 238);
-            this.q3Output.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.q3Output.Location = new System.Drawing.Point(77, 568);
+            this.q3Output.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.q3Output.Name = "q3Output";
-            this.q3Output.Size = new System.Drawing.Size(156, 20);
+            this.q3Output.Size = new System.Drawing.Size(416, 48);
             this.q3Output.TabIndex = 27;
             this.q3Output.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
@@ -57,10 +57,10 @@ namespace ArrayExamples
             this.q3Label.AutoSize = true;
             this.q3Label.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.q3Label.ForeColor = System.Drawing.Color.White;
-            this.q3Label.Location = new System.Drawing.Point(26, 222);
-            this.q3Label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.q3Label.Location = new System.Drawing.Point(69, 529);
+            this.q3Label.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.q3Label.Name = "q3Label";
-            this.q3Label.Size = new System.Drawing.Size(63, 13);
+            this.q3Label.Size = new System.Drawing.Size(146, 37);
             this.q3Label.TabIndex = 26;
             this.q3Label.Text = "Question 3";
             // 
@@ -69,10 +69,10 @@ namespace ArrayExamples
             this.titleLabel.BackColor = System.Drawing.Color.DarkGoldenrod;
             this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.titleLabel.ForeColor = System.Drawing.Color.White;
-            this.titleLabel.Location = new System.Drawing.Point(-1, -1);
-            this.titleLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.titleLabel.Location = new System.Drawing.Point(-3, -2);
+            this.titleLabel.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.titleLabel.Name = "titleLabel";
-            this.titleLabel.Size = new System.Drawing.Size(219, 52);
+            this.titleLabel.Size = new System.Drawing.Size(584, 124);
             this.titleLabel.TabIndex = 24;
             this.titleLabel.Text = "Array Examples";
             this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -82,10 +82,10 @@ namespace ArrayExamples
             this.q2Output.BackColor = System.Drawing.Color.White;
             this.q2Output.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.q2Output.ForeColor = System.Drawing.Color.Black;
-            this.q2Output.Location = new System.Drawing.Point(29, 189);
-            this.q2Output.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.q2Output.Location = new System.Drawing.Point(77, 451);
+            this.q2Output.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.q2Output.Name = "q2Output";
-            this.q2Output.Size = new System.Drawing.Size(156, 20);
+            this.q2Output.Size = new System.Drawing.Size(416, 48);
             this.q2Output.TabIndex = 23;
             this.q2Output.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
@@ -94,10 +94,10 @@ namespace ArrayExamples
             this.q2Label.AutoSize = true;
             this.q2Label.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.q2Label.ForeColor = System.Drawing.Color.White;
-            this.q2Label.Location = new System.Drawing.Point(26, 173);
-            this.q2Label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.q2Label.Location = new System.Drawing.Point(69, 413);
+            this.q2Label.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.q2Label.Name = "q2Label";
-            this.q2Label.Size = new System.Drawing.Size(63, 13);
+            this.q2Label.Size = new System.Drawing.Size(146, 37);
             this.q2Label.TabIndex = 22;
             this.q2Label.Text = "Question 2";
             // 
@@ -106,10 +106,10 @@ namespace ArrayExamples
             this.q1Output.BackColor = System.Drawing.Color.White;
             this.q1Output.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.q1Output.ForeColor = System.Drawing.Color.Black;
-            this.q1Output.Location = new System.Drawing.Point(29, 143);
-            this.q1Output.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.q1Output.Location = new System.Drawing.Point(77, 341);
+            this.q1Output.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.q1Output.Name = "q1Output";
-            this.q1Output.Size = new System.Drawing.Size(156, 20);
+            this.q1Output.Size = new System.Drawing.Size(416, 48);
             this.q1Output.TabIndex = 21;
             this.q1Output.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
@@ -118,10 +118,10 @@ namespace ArrayExamples
             this.q1Label.AutoSize = true;
             this.q1Label.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.q1Label.ForeColor = System.Drawing.Color.White;
-            this.q1Label.Location = new System.Drawing.Point(26, 126);
-            this.q1Label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.q1Label.Location = new System.Drawing.Point(69, 300);
+            this.q1Label.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.q1Label.Name = "q1Label";
-            this.q1Label.Size = new System.Drawing.Size(63, 13);
+            this.q1Label.Size = new System.Drawing.Size(146, 37);
             this.q1Label.TabIndex = 20;
             this.q1Label.Text = "Question 1";
             // 
@@ -131,10 +131,10 @@ namespace ArrayExamples
             this.runButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.runButton.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.runButton.ForeColor = System.Drawing.Color.White;
-            this.runButton.Location = new System.Drawing.Point(29, 79);
-            this.runButton.Margin = new System.Windows.Forms.Padding(2);
+            this.runButton.Location = new System.Drawing.Point(77, 188);
+            this.runButton.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.runButton.Name = "runButton";
-            this.runButton.Size = new System.Drawing.Size(156, 25);
+            this.runButton.Size = new System.Drawing.Size(416, 60);
             this.runButton.TabIndex = 19;
             this.runButton.Text = "Run";
             this.runButton.UseVisualStyleBackColor = false;
@@ -142,10 +142,10 @@ namespace ArrayExamples
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(216, 301);
+            this.ClientSize = new System.Drawing.Size(581, 718);
             this.Controls.Add(this.q3Output);
             this.Controls.Add(this.q3Label);
             this.Controls.Add(this.titleLabel);
@@ -155,6 +155,7 @@ namespace ArrayExamples
             this.Controls.Add(this.q1Label);
             this.Controls.Add(this.runButton);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Arrays";
